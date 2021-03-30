fa069f36503d08ce05b32a84df5be37fe0e672d7
AnushaShetty <anushashetty669@gmail.com>
2021-03-30 18:53:21 +0530

    updating notes, fetching notes

--- backend/app.py
+++ backend/app.py
@@ -2,27 +2,37 @@ from flask import Flask, jsonify,request
 from flask_cors import CORS, cross_origin
 import content as cnt
 import json
-from pprint import pprint
 app = Flask(__name__)
 cors = CORS(app)
 
-# render all questions in maths subject whose chapterName is specified - learning in maths
-@app.route('/learnig/maths/chapterName')
-def all_questions_in_chapter_of_maths():
-    result = cnt.all_questions_in_chapter_of_maths(chapterName = "Conic Section")
-    pprint(result)
+# render all questions in maths subject whose chapterName is specified - learning and exam in maths
+@app.route('/learnig/maths/<chapterName>')
+def all_questions_in_chapter_of_maths(chapterName):
+    result = cnt.all_questions_in_chapter_of_maths(chapterName)
     return jsonify({"result":result})
 
 # pick only few questions in each chapter - exam in maths
 @app.route('/exam/maths')
 def specific_question_in_chapter_maths():
     num_quest = {"Conic Section":2}     # from each chapter how many questions need to be picked
-    res = []
     for chapter in num_quest:
-        res.extend( cnt.specific_question_in_chapter_maths(chapter,num_quest[chapter]) )
-    pprint(res)
-    return jsonify({"result":res})
+        ques = ( cnt.specific_question_in_chapter_maths(chapter,num_quest[chapter]) )
+    return jsonify({"result":ques})
 
+# based on specified subject and chapter name notes is updated - learning
+@app.route('/learning/<subject>/<chapterName>/notes/<userid>',methods=['POST','GET'])
+def store_notes_of_chapter(subject,chapterName,userid):
+    if request.method == 'POST':
+        notes = request.form['notes']   # <form> <p name="notes"> ...  </p> </form>
+        res = cnt.store_notes_of_chapter(userid,subject,chapterName,notes)
+    res = cnt.get_notes(userid,subject,chapterName)     # updated notes is obtained not necessary to be sent
+    return jsonify({"notes":res})
+
+@app.route('/learning/<subject>/<chapterName>/getnotes/<userid>')
+def get_notes(subject, chapterName, userid):
+    res = cnt.get_notes(userid,subject,chapterName)     # updated notes is obtained not necessary to be sent
+    return jsonify({"notes":res})
+    
 app.run()
 # all_questions_choosen_chapter()
 # specific_question_in_chapter()
--- backend/content.py
+++ backend/content.py
@@ -6,21 +6,36 @@ from random import randint
 db = MongoClient()
 mydb = db.Exam_Analyzer_DB
 math_collection = mydb['Mathematics']
+user_collection = mydb["UserData"]
+
+# exam section
 
 def all_questions_in_chapter_of_maths(chapterName = "Conic Section"):
     q = math_collection.find({ "ChapterName": chapterName },{"_id":0})
     return [ele for ele in q]
 
 def specific_question_in_chapter_maths(chapterName, num_quest):
-    res = []
+    ques= []
     for i in range(num_quest):
-        i = randint(1,61)
+        i = randint(1,20)
         q = math_collection.aggregate([
             {"$match":{"ChapterName":chapterName}},
             {"$unwind": "$questionSet"},
             {"$match": {"questionSet.questionNumber":i}},
             {"$project": {"_id":0}}
         ])
-        res.append([ele for ele in q])
-        pprint(res)
-    return res
+        ques.append([ele for ele in q])
+    return ques
+
+# learning
+
+def store_notes_of_chapter(userid,subject,chapterName,notes):
+    result = user_collection.update(
+        {"userid":userid, "progress.subject":subject,"progress.chapterName":chapterName},
+        {'$set': {"progress.$.notes":notes}}
+    )
+
+def get_notes(userid,subject,chapterName):
+    res = user_collection.find(
+        {"userid":userid,"progress.subject":subject,"progress.chapterName":chapterName},{"_id":0,"progress.notes":1})
+    return [ele for ele in res]
