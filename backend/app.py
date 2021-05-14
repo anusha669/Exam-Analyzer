eb41dd62c173cc6c08d65ed5657353068806eff3
AnushaShetty <anushashetty669@gmail.com>
2021-05-14 20:19:13 +0530

    fixed notes related functionality

--- backend/app.py
+++ backend/app.py
@@ -5,18 +5,20 @@ import json
 app = Flask(__name__)
 cors = CORS(app)
 
-# render all questions in maths subject whose chapterName is specified - learning and exam in maths
-@app.route('/learnig/maths/<chapterName>')
-def all_questions_in_chapter_of_maths(chapterName):
-    result = cnt.all_questions_in_chapter_of_maths(chapterName)
+# render specific num of questns in maths subject whose chapterName is specified - practice maths
+@app.route('/practice/maths/<chapterName>/<num_quest>')
+def all_questions_in_chapter_of_maths(chapterName, num_quest):
+    result = cnt.all_questions_in_chapter_of_maths(chapterName,int(num_quest) )
+    print(result)
     return jsonify({"result":result})
 
 # pick only few questions in each chapter - exam in maths
 @app.route('/exam/maths')
 def specific_question_in_chapter_maths():
-    num_quest = {"Conic Section":2}     # from each chapter how many questions need to be picked
+    num_quest = {"Conic Section":3}     # from each chapter how many questions need to be picked
+    ques = []
     for chapter in num_quest:
-        ques = ( cnt.specific_question_in_chapter_maths(chapter,num_quest[chapter]) )
+        ques = cnt.specific_question_in_chapter_maths(chapter,num_quest[chapter]) 
     return jsonify({"result":ques})
 
 # based on specified subject and chapter name notes is updated - learning
@@ -24,15 +26,21 @@ def specific_question_in_chapter_maths():
 def store_notes_of_chapter(subject,chapterName,userid):
     if request.method == 'POST':
         notes = request.form['notes']   # <form> <p name="notes"> ...  </p> </form>
-        res = cnt.store_notes_of_chapter(userid,subject,chapterName,notes)
-    res = cnt.get_notes(userid,subject,chapterName)     # updated notes is obtained not necessary to be sent
+        cnt.store_notes_of_chapter(userid,subject[0]+"_"+chapterName,notes)
+    res = cnt.get_notes(userid,subject[0]+"_"+chapterName)     # updated notes is obtained not necessary to be sent
     return jsonify({"notes":res})
 
 @app.route('/learning/<subject>/<chapterName>/getnotes/<userid>')
 def get_notes(subject, chapterName, userid):
-    res = cnt.get_notes(userid,subject,chapterName)     # updated notes is obtained not necessary to be sent
+    res = cnt.get_notes(userid,subject+"_"+chapterName)     # updated notes is obtained not necessary to be sent
     return jsonify({"notes":res})
     
+# take a test, produce test report with data analytics
+
+
+
+
+
 app.run()
 # all_questions_choosen_chapter()
 # specific_question_in_chapter()
--- backend/content.py
+++ backend/content.py
@@ -8,34 +8,38 @@ mydb = db.Exam_Analyzer_DB
 math_collection = mydb['Mathematics']
 user_collection = mydb["UserData"]
 
-# exam section
+# practice section
 
-def all_questions_in_chapter_of_maths(chapterName = "Conic Section"):
-    q = math_collection.find({ "ChapterName": chapterName },{"_id":0})
+def all_questions_in_chapter_of_maths(chapterName,num_quest):
+    q = math_collection.aggregate([
+    {'$match':{"ChapterName":chapterName}},
+    {'$unwind':"$questionSet"},{"$project": {"_id":0}},
+    { "$sample": { "size": num_quest }}
+    ])
     return [ele for ele in q]
+
+# exam section
 
 def specific_question_in_chapter_maths(chapterName, num_quest):
     ques= []
-    for i in range(num_quest):
-        i = randint(1,20)
-        q = math_collection.aggregate([
+    q = math_collection.aggregate([
             {"$match":{"ChapterName":chapterName}},
             {"$unwind": "$questionSet"},
-            {"$match": {"questionSet.questionNumber":i}},
-            {"$project": {"_id":0}}
-        ])
-        ques.append([ele for ele in q])
+            {"$project": {"_id":0}},
+            { "$sample": { "size": num_quest } }
+    ])
+    ques.append([ele for ele in q])
     return ques
 
 # learning
 
-def store_notes_of_chapter(userid,subject,chapterName,notes):
+def store_notes_of_chapter(userid,chapterName,notes):
     result = user_collection.update(
-        {"userid":userid, "progress.subject":subject,"progress.chapterName":chapterName},
+        {"userid":userid, "progress.chapterName":chapterName},
         {'$set': {"progress.$.notes":notes}}
     )
 
-def get_notes(userid,subject,chapterName):
+def get_notes(userid,chapterName):
     res = user_collection.find(
-        {"userid":userid,"progress.subject":subject,"progress.chapterName":chapterName},{"_id":0,"progress.notes":1})
+        {"userid":userid,"progress.chapterName":chapterName},{"_id":0,"progress.$.notes":1})
     return [ele for ele in res]
